c5cc480e7313043423fe3395fffffff4931403aa
Oleg <oyshoboy@gmail.com>
2021-01-04 18:08:07 -0800

    bla bla bla

--- Assets/Imports/_KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
+++ Assets/Imports/_KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
@@ -19,6 +19,8 @@ namespace KinematicCharacterController.Examples
 
         private void Start()
         {
+            
+            Debug.Log("Example Playe script started");
             Cursor.lockState = CursorLockMode.Locked;
 
             // Tell camera to follow transform
